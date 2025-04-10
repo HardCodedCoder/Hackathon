6b73ea6c4363c36af4349c17b6d3ed1c571fa408
Andreas <2210859005@fh-burgenland.at>
2025-04-10 12:51:42 +0200

    Add a simple buffet and food interaction

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from board import Board
 from player import Player
 from controlUI import ControlUI
 from tables import Table 
+from buffet import Buffet
 
 WIDTH = 1200
 HEIGHT = 800
@@ -51,7 +52,21 @@ def init_controlUI(board: Board) -> ControlUI:
     control_ui = ControlUI(board.ui_area_rect)
     return control_ui
 
-def run_gameloop(board: Board, player: Player, clock: pygame.time.Clock, screen: pygame.Surface, control_ui: ControlUI) -> None:
+def init_buffet(board: Board) -> Buffet:
+    """
+    Initializes the buffet on the right side of the game area.
+    :param board: Board object
+    :return: Buffet object with a given food item.
+    """
+    buffet_width, buffet_height = 100, 100
+    # Position the buffet within the game area: 20px from the right edge with an offset from the top.
+    buffet_x = board.game_area_rect.right - buffet_width - 20
+    buffet_y = board.game_area_rect.top + 100
+    buffet_rect = pygame.Rect(buffet_x, buffet_y, buffet_width, buffet_height)
+    return Buffet(buffet_rect, "Pizza")
+
+def run_gameloop(board: Board, player: Player, clock: pygame.time.Clock, 
+                 screen: pygame.Surface, control_ui: ControlUI, buffet: Buffet) -> None:
     running = True
     while running:
         clock.tick(FPS)
@@ -65,12 +80,20 @@ def run_gameloop(board: Board, player: Player, clock: pygame.time.Clock, screen:
         if (player is not None):
             player.update(keys)
 
-        # Zeichne das Board und den Spieler
-        
+        # Check for buffet interaction: if player collides with the buffet and presses "E"
+        if keys[pygame.K_e] and player.rect.colliderect(buffet.rect) and not buffet.taken:
+            buffet.taken = True
+            control_ui.food_ordered = buffet.food
+            # TODO: set player.carrying_food = buffet.food
+            
+        # draw the board and the player
         board.draw(screen)
         if (player is not None):
             player.draw(screen)
         control_ui.draw(screen)
+        
+        # Draw the buffet
+        buffet.draw(screen)
         
         pygame.display.flip()
 
@@ -80,6 +103,7 @@ def main():
     board = init_board(width=WIDTH,height=HEIGHT)
     player = init_player(board=board)
     controlUI = init_controlUI(board=board)
+    buffet = init_buffet(board)
     
     clock = pygame.time.Clock()
     
@@ -94,7 +118,7 @@ def main():
         table.draw(screen)
     
     # main game loop
-    run_gameloop(board=board, player=player, clock=clock, screen=screen, control_ui=controlUI)
+    run_gameloop(board=board, player=player, clock=clock, screen=screen, control_ui=controlUI, buffet=buffet)
 
     pygame.quit()
     sys.exit()
